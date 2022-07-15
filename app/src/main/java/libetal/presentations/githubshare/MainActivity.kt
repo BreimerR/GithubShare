f85506f96a2c107362a429c80385ada93429f240
brymher <brymher@gmail.com>
2022-07-15 11:57:05 +0300

    Updated user interface

--- app/src/main/java/libetal/presentations/githubshare/MainActivity.kt
+++ app/src/main/java/libetal/presentations/githubshare/MainActivity.kt
@@ -3,11 +3,18 @@ package libetal.presentations.githubshare
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import libetal.presentations.githubshare.ui.theme.GithubShareTheme
@@ -31,7 +38,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    var user by remember { mutableStateOf(name) }
+    Row(horizontalArrangement = Arrangement.Center) {
+        Button(onClick = {
+            user = "Button"
+        }) {
+            Text(text = "Hello $user!")
+        }
+    }
 }
 
 @Preview(showBackground = true)
